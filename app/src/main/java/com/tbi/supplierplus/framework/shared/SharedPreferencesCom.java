08807e75523ffa2e10789278768a8e66b7fc537b
TBIPC <yousifTBI>
2023-05-28 15:28:55 +0300

    first Commitxx2

--- app/src/main/java/com/tbi/supplierplus/framework/shared/SharedPreferencesCom.java
+++ app/src/main/java/com/tbi/supplierplus/framework/shared/SharedPreferencesCom.java
@@ -10,17 +10,15 @@ public class SharedPreferencesCom {
     private SharedPreferences sharedPreferencesLogIn;
     private SharedPreferences.Editor edits;
 
-    public SharedPreferencesCom()
-    {
+    public SharedPreferencesCom() {
     }
 
-    public static void init(Context context)
-    {
+    public static void init(Context context) {
         Instance = new SharedPreferencesCom(context.getApplicationContext());
+
     }
 
-    public static SharedPreferencesCom getInstance()
-    {
+    public static SharedPreferencesCom getInstance() {
         if (null == Instance)
             Instance = new SharedPreferencesCom();
         return Instance;
@@ -31,30 +29,30 @@ public class SharedPreferencesCom {
         edits = sharedPreferencesLogIn.edit();
     }
 
-    public void remove()
-    {
+    public void remove() {
         sharedPreferencesLogIn.edit().clear().commit();
     }
 
-    public void setSharedDistributor_ID(String number)
-    {
+    public void setSharedDistributor_ID(String number
+    ) {
         edits.putString("Distributor_ID", number);
         edits.apply();
     }
-    public void setSharedUser_ID(String number)
-    {
+    public void setSharedUser_ID(String number
+    ) {
         edits.putString("User_ID", number);
         edits.apply();
     }
 
 
-    public String gerSharedDistributor_ID()
-    {
+    public String gerSharedDistributor_ID(
+    ) {
         String phoneNumber = sharedPreferencesLogIn.getString("Distributor_ID", "");
         return phoneNumber;
+
     }
-    public String gerSharedUser_ID()
-    {
+    public String gerSharedUser_ID(
+    ) {
         String phoneNumber = sharedPreferencesLogIn.getString("User_ID", "");
         return phoneNumber;
 
